99a109acf779eacdd2a09fb0e5b71b7a7a5d2946
AmineHammoutene10 <aminehammoutene10@gmail.com>
2025-01-22 09:57:18 +0100

    Symphony init W Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+
+const App = () => {
+  return (
+    <div>
+      {/* Utilisation de la Navbar */}
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+// src/components/Navbar.jsx
+import React from "react";
+import "./../styles/navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="/src/assets/logo.png" alt="MyBank Logo" />
+        <h1>MyBank</h1>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#about">About Us</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="navbar-buttons">
+        <button className="btn-login">Login</button>
+        <button className="btn-signup">Sign Up</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
